1f91a77a9ba97f3ab54dd0d11c5223f356925255
Andrey Petelin <am.petelin@gmail.com>
2025-04-16 10:00:14 +0500

    refactor: index optimization and code reformatting

--- view.py
+++ view.py
@@ -1,15 +1,17 @@
-import re
-import os
 import argparse
-import geoip2.database
+import os
+import re
 import urllib.request
 from collections import defaultdict
 
-# Глобальный кэш для хранения результатов get_region_and_asn по IP
+import geoip2.database
+
 region_asn_cache = {}
+
 
 def clear_screen():
     os.system('clear' if os.name == 'posix' else 'cls')
+
 
 def download_geoip_db(db_url, db_path):
     if os.path.exists(db_path):
@@ -18,6 +20,7 @@ def download_geoip_db(db_url, db_path):
     print(f"\033[92mСкачивание базы данных из\033[0m {db_url}...")
     urllib.request.urlretrieve(db_url, db_path)
     print("\033[92mЗагрузка завершена.\033[0m")
+
 
 def parse_log_entry(log, filter_ip_resource, city_reader, asn_reader):
     pattern = re.compile(
@@ -32,59 +35,68 @@ def parse_log_entry(log, filter_ip_resource, city_reader, asn_reader):
         email = match.group("email")
         resource = match.group("resource")
         destination = match.group("destination")
-        
-        # Фильтрация по IP и ресурсу
+
         if filter_ip_resource:
             if re.match(r"^(?:\d{1,3}\.){3}\d{1,3}$", resource) or \
-               re.match(r"^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$", resource):
+                    re.match(r"^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$", resource):
                 return None
         else:
             if re.match(r"^(?:\d{1,3}\.){3}\d{1,3}$", resource) or \
-               re.match(r"^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$", resource):
+                    re.match(r"^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$", resource):
                 region_asn = get_region_and_asn(resource, city_reader, asn_reader)
-                country = region_asn.split(",")[0]  # Получаем страну из строки
+                country = region_asn.split(",")[0]
                 if country in {"Russia", "Belarus"}:
-                    resource = f"\033[91m{resource} ({country})\033[0m"  # Подсветка красным
+                    resource = f"\033[91m{resource} ({country})\033[0m"
                 else:
                     resource = f"{resource} ({country})"
-        
+
         return ip, email, resource, destination
     return None
+
 
 def extract_email_number(email):
     match = re.match(r"(\d+)\..*", email)
     return int(match.group(1)) if match else email
 
+
 def highlight_email(email):
     return f"\033[92m{email}\033[0m"
+
 
 def highlight_ip(ip):
     return f"\033[94m{ip}\033[0m"
 
+
 def highlight_resource(resource):
     highlight_domains = {
-        "mycdn.me", "mvk.com", "userapi.com", "vk-apps.com", "vk-cdn.me", "vk-cdn.net", "vk-portal.net", "vk.cc", "vk.com", "vk.company",
-        "vk.design", "vk.link", "vk.me", "vk.team", "vkcache.com", "vkgo.app", "vklive.app", "vkmessenger.app", "vkmessenger.com", "vkuser.net",
+        "mycdn.me", "mvk.com", "userapi.com", "vk-apps.com", "vk-cdn.me", "vk-cdn.net", "vk-portal.net", "vk.cc",
+        "vk.com", "vk.company",
+        "vk.design", "vk.link", "vk.me", "vk.team", "vkcache.com", "vkgo.app", "vklive.app", "vkmessenger.app",
+        "vkmessenger.com", "vkuser.net",
         "vkuseraudio.com", "vkuseraudio.net", "vkuserlive.net", "vkuservideo.com", "vkuservideo.net",
-        "yandex.aero", "yandex.az", "yandex.by", "yandex.co.il", "yandex.com", "yandex.com.am", "yandex.com.ge", "yandex.com.ru", "yandex.com.tr",
-        "yandex.com.ua", "yandex.de", "yandex.ee", "yandex.eu", "yandex.fi", "yandex.fr", "yandex.jobs", "yandex.kg", "yandex.kz", "yandex.lt",
-        "yandex.lv", "yandex.md", "yandex.net", "yandex.org", "yandex.pl", "yandex.ru", "yandex.st", "yandex.sx", "yandex.tj", "yandex.tm",
+        "yandex.aero", "yandex.az", "yandex.by", "yandex.co.il", "yandex.com", "yandex.com.am", "yandex.com.ge",
+        "yandex.com.ru", "yandex.com.tr",
+        "yandex.com.ua", "yandex.de", "yandex.ee", "yandex.eu", "yandex.fi", "yandex.fr", "yandex.jobs", "yandex.kg",
+        "yandex.kz", "yandex.lt",
+        "yandex.lv", "yandex.md", "yandex.net", "yandex.org", "yandex.pl", "yandex.ru", "yandex.st", "yandex.sx",
+        "yandex.tj", "yandex.tm",
         "yandex.ua", "yandex.uz", "yandexcloud.net", "yastatic.net"
     }
-    
+
     questinable_domains = {
         "kaspersky-labs.com", "kaspersky.com"
     }
 
     if any(resource == domain or resource.endswith("." + domain) for domain in highlight_domains) \
-       or re.search(r"\.ru$|\.su$|\.by$|[а-яА-Я]", resource) \
-       or "xn--" in resource:
+            or re.search(r"\.ru$|\.su$|\.by$|[а-яА-Я]", resource) \
+            or "xn--" in resource:
         return f"\033[91m{resource}\033[0m"
-    
+
     if any(resource == domain or resource.endswith("." + domain) for domain in questinable_domains):
         return f"\033[38;5;186m{resource}\033[0m"
 
     return resource
+
 
 def get_region_and_asn(ip, city_reader, asn_reader):
     if ip in region_asn_cache:
@@ -95,16 +107,17 @@ def get_region_and_asn(ip, city_reader, asn_reader):
         region = city_response.subdivisions.most_specific.name if city_response.subdivisions.most_specific.name else "Unknown Region"
     except Exception:
         country, region = "Unknown Country", "Unknown Region"
-    
+
     try:
         asn_response = asn_reader.asn(ip)
         asn = f"AS{asn_response.autonomous_system_number} {asn_response.autonomous_system_organization}"
     except Exception:
         asn = "Unknown ASN"
-    
+
     result = f"{country}, {region}, {asn}"
     region_asn_cache[ip] = result
     return result
+
 
 def process_logs(logs_iterator, city_reader, asn_reader, filter_ip_resource):
     data = defaultdict(lambda: defaultdict(dict))
@@ -115,6 +128,7 @@ def process_logs(logs_iterator, city_reader, asn_reader, filter_ip_resource):
             region_asn = get_region_and_asn(ip, city_reader, asn_reader)
             data[email].setdefault(ip, {"region_asn": region_asn, "resources": {}})["resources"][resource] = destination
     return data
+
 
 def process_summary(logs_iterator, city_reader, asn_reader, filter_ip_resource):
     summary = defaultdict(set)
@@ -127,6 +141,7 @@ def process_summary(logs_iterator, city_reader, asn_reader, filter_ip_resource):
             regions[ip] = get_region_and_asn(ip, city_reader, asn_reader)
     return {email: (ips, regions) for email, ips in summary.items()}
 
+
 def print_sorted_logs(data):
     for email in sorted(data.keys(), key=extract_email_number):
         print(f"Email: {highlight_email(email)}")
@@ -134,6 +149,7 @@ def print_sorted_logs(data):
             print(f"  IP: {highlight_ip(ip)} ({info['region_asn']})")
             for resource, destination in sorted(info["resources"].items()):
                 print(f"    Resource: {highlight_resource(resource)} -> [{destination}]")
+
 
 def print_summary(summary):
     for email in sorted(summary.keys(), key=extract_email_number):
@@ -144,6 +160,7 @@ def print_summary(summary):
         for ip in sorted(ips):
             print(f"  IP: {highlight_ip(ip)} ({regions[ip]})")
 
+
 def extract_ip_from_foreign(foreign):
     m = re.match(r"^(\d+\.\d+\.\d+\.\d+):\d+$", foreign)
     if m:
@@ -153,8 +170,8 @@ def extract_ip_from_foreign(foreign):
         return parts[0]
     return foreign
 
+
 def process_online_mode(logs_iterator, city_reader, asn_reader):
-    # Формируем отображение: IP -> последний email (из логов)
     ip_last_email = {}
     for log in logs_iterator:
         parsed = parse_log_entry(log, filter_ip_resource=False, city_reader=city_reader, asn_reader=asn_reader)
@@ -193,15 +210,19 @@ def process_online_mode(logs_iterator, city_reader, asn_reader):
     else:
         print("Нет ESTABLISHED соединений, найденных в логах.")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--summary", action="store_true", help="Вывести только email, количество уникальных IP и сами IP с регионами и ASN")
+    parser.add_argument("--summary", action="store_true",
+                        help="Вывести только email, количество уникальных IP и сами IP с регионами и ASN")
     parser.add_argument("--ip", action="store_true", help="Вывести не только домены, но и ip")
-    parser.add_argument("--online", action="store_true", help="Показать ESTABLISHED соединения (из логов) с последним email доступа")
+    parser.add_argument("--online", action="store_true",
+                        help="Показать ESTABLISHED соединения (из логов) с последним email доступа")
     args = parser.parse_args()
 
     default_log_file_path = "/var/lib/marzban/access.log"
-    user_input_path = input(f"Укажите путь до логов (нажмите Enter для использования '{default_log_file_path}'): ").strip()
+    user_input_path = input(
+        f"Укажите путь до логов (нажмите Enter для использования '{default_log_file_path}'): ").strip()
     log_file_path = user_input_path if user_input_path else default_log_file_path
 
     if log_file_path == default_log_file_path:
@@ -213,22 +234,22 @@ if __name__ == "__main__":
     asn_db_path = "/tmp/GeoLite2-ASN.mmdb"
     city_db_url = "https://git.io/GeoLite2-City.mmdb"
     asn_db_url = "https://git.io/GeoLite2-ASN.mmdb"
-    
+
     download_geoip_db(city_db_url, city_db_path)
-    download_geoip_db(asn_db_url, asn_db_path)    
+    download_geoip_db(asn_db_url, asn_db_path)
 
     with geoip2.database.Reader(city_db_path) as city_reader, geoip2.database.Reader(asn_db_path) as asn_reader:
         filter_ip_resource = True
         if args.ip:
             filter_ip_resource = False
         clear_screen()
-        
+
         if args.online:
             filter_ip_resource = False
             with open(log_file_path, "r") as file:
                 process_online_mode(file, city_reader, asn_reader)
             exit(0)
-            
+
         if args.summary:
             filter_ip_resource = False
             with open(log_file_path, "r") as file:
